fa9224dee89e47a08e41148fd53dfe0cc2d5ec9b
zqlovejyc <943620963@qq.com>
2018-02-22 17:45:40 +0800

    select组件新增自定义input输入框

--- example/select/index.js
+++ example/select/index.js
@@ -2,32 +2,49 @@ var Zan = require('../../dist/index');
 
 Page(Object.assign({}, Zan.CheckLabel, {
 
-  data: {
-    items: [
-      {
-        padding: 0,
-        value: '1',
-        name: '选项一',
-      },
-      {
-        padding: 0,
-        value: '2',
-        name: '选项二',
-      },
-    ],
+    data: {
+        items: [
+            {
+                padding: 0,
+                value: '1',
+                name: '选项一',
+                bindinput: 'bindInput'
+            },
+            {
+                padding: 0,
+                value: '2',
+                name: '选项二'
+            },
+        ],
+        items1: [
+            {
+                padding: 0,
+                value: '1',
+                name: '选项一'
+            },
+            {
+                padding: 0,
+                value: '2',
+                name: '选项二'
+            },
+        ],
+        checked: {
+            base: -1,
+            color: -1,
+            custom: -1,
+            input: -1,
+            left: -1
+        },
 
-    checked: {
-      base: -1,
-      color: -1,
-      custom:-1
+        activeColor: '#4b0'
     },
 
-    activeColor: '#4b0'
-  },
-
-  handleZanSelectChange({ componentId, value }) {
-    this.setData({
-      [`checked.${componentId}`]: value
-    });
-  }
+    handleZanSelectChange({ componentId, value }) {
+        this.setData({
+            [`checked.${componentId}`]: value
+        });
+    },
+    bindInput(e) {
+        console.log(e.detail.value);
+    }
 }));
